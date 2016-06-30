ec18f3ea9d7db094f5e8d1669117a721cad99a83
dukakisxyz <kdukakis@icloud.com>
2016-06-30 15:36:34 +0300

    bug fixes

--- server.py
+++ server.py
@@ -14,32 +14,23 @@ app = flask.Flask(__name__)
 payment = Payment(app, Wallet())
 
 
-
-
-
 @app.route('/make_password/<length>')
 @payment.required(1000)
 def make_password(length):
-	
 	#length = int(length)
-
 	dice_pass = {
 	"password" : []
 }
 	password = ""
 	for x in range(length):
-		word = make_word()
+		word_in_numbers = ""
+		for roll in range (0,5):
+			number = str(randint(1,6))
+			word_in_numbers = word_in_numbers + number
+		word_in_numbers = word_in_numbers
+		word = dictionary[word_in_numbers]
 		dice_pass["password"].append(word)
-	return (dice_pass)
-
-def make_word():
-	word_in_numbers = ""
-	for roll in range (0,5):
-		number = str(randint(1,6))
-		word_in_numbers = word_in_numbers + number
-	word_in_numbers = word_in_numbers
-	word = dictionary[word_in_numbers]
-	return (word)
+	return json.dumps(dice_pass)
 
 '''
 @app.route('/manifest')
